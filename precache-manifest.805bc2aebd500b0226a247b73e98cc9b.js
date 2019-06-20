4b297930d341625537918d212ab7c0ba002d59db
yashua <yashuab.abraham@gmail.com>
2019-06-20 01:09:05 -0400

    Updates

---
+++ precache-manifest.805bc2aebd500b0226a247b73e98cc9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca4413c1f81701cf94e12daa4ad948c",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "7a20486d1358a83ca1dc",
+    "url": "/myapp/static/css/main.bd39fc1f.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/myapp/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7a20486d1358a83ca1dc",
+    "url": "/myapp/static/js/main.074bc821.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
